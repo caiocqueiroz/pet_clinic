832344759864c768a43bcecab4b1cb012aecac50
Caio Queiroz <ccesar.qp@gmail.com>
2024-01-31 20:48:16 -0300

    fix tf folder (infrastructure)

--- infrastructure/ec2.tf
+++ infrastructure/ec2.tf
@@ -1,32 +1,32 @@
 resource "aws_launch_template" "ecs_lt" {
-    name = "ecs-template"
-    image_id = "ami-0277155c3f0ab2930"
-    instance_type = "t3.micro"
+  name          = "ecs-template"
+  image_id      = "ami-0277155c3f0ab2930"
+  instance_type = "t3.micro"
 
-    key_name = aws_key_pair.webserver_key.key_name
-    vpc_security_group_ids = [aws_security_group.security_group.id]
-    iam_instance_profile {
-      name = aws_iam_instance_profile.ecs_instance_profile.name
+  key_name               = aws_key_pair.webserver_key.key_name
+  vpc_security_group_ids = [aws_security_group.security_group.id]
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ecs_instance_profile.name
+  }
+
+  block_device_mappings {
+    device_name = "/dev/xvda"
+    ebs {
+      volume_size = 30
+      volume_type = "gp2"
     }
-  
-    block_device_mappings {
-      device_name = "/dev/xvda"
-      ebs {
-        volume_size = 30
-        volume_type = "gp2"
-      }
+  }
+
+  tag_specifications {
+    resource_type = "instance"
+    tags = {
+      Name = "ecs-instance"
     }
+  }
 
-    tag_specifications {
-      resource_type = "instance"
-      tags = {
-        Name = "ecs-instance"
-      }
-    }
+  user_data = filebase64("${path.module}/ecs.sh")
 
-    user_data = filebase64("${path.module}/ecs.sh")
-
-    depends_on = [ aws_iam_instance_profile.ecs_instance_profile ]
+  depends_on = [aws_iam_instance_profile.ecs_instance_profile]
 }
 
 resource "aws_key_pair" "webserver_key" {
@@ -62,56 +62,56 @@ resource "aws_iam_instance_profile" "ecs_instance_profile" {
 }
 
 resource "aws_autoscaling_group" "ecs_asg" {
-    vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
-    desired_capacity = 2
-    min_size = 1
-    max_size = 3
-  
+  vpc_zone_identifier = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+  desired_capacity    = 2
+  min_size            = 1
+  max_size            = 3
+
   launch_template {
-    id = aws_launch_template.ecs_lt.id
+    id      = aws_launch_template.ecs_lt.id
     version = "$Latest"
   }
 
   tag {
-    key = "AmazonECSManaged"
-    value = true
+    key                 = "AmazonECSManaged"
+    value               = true
     propagate_at_launch = true
   }
-  depends_on = [ aws_launch_template.ecs_lt ]
+  depends_on = [aws_launch_template.ecs_lt]
 }
 
 
 resource "aws_lb" "ecs_alb" {
- name               = "ecs-alb"
- internal           = false
- load_balancer_type = "application"
- security_groups    = [aws_security_group.security_group.id]
- subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
+  name               = "ecs-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.security_group.id]
+  subnets            = [aws_subnet.subnet.id, aws_subnet.subnet2.id]
 
- tags = {
-   Name = "ecs-alb"
- }
+  tags = {
+    Name = "ecs-alb"
+  }
 }
 
 resource "aws_lb_listener" "ecs_alb_listener" {
- load_balancer_arn = aws_lb.ecs_alb.arn
- port              = 80
- protocol          = "HTTP"
+  load_balancer_arn = aws_lb.ecs_alb.arn
+  port              = 80
+  protocol          = "HTTP"
 
- default_action {
-   type             = "forward"
-   target_group_arn = aws_lb_target_group.ecs_tg.arn
- }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.ecs_tg.arn
+  }
 }
 
 resource "aws_lb_target_group" "ecs_tg" {
- name        = "ecs-target-group"
- port        = 80
- protocol    = "HTTP"
- target_type = "ip"
- vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+  name        = "ecs-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
 
- health_check {
-   path = "/"
- }
+  health_check {
+    path = "/"
+  }
 }
