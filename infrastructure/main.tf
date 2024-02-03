2efd9971e29ac5eee56d0a8a113e1836efea53f9
Caio Queiroz <ccesar.qp@gmail.com>
2024-02-03 08:37:45 -0300

    add role ecs attch

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -27,6 +27,11 @@ resource "aws_iam_role" "ecsiamrole" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
+  role       = aws_iam_role.ecsiamrole.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
 resource "aws_ecs_task_definition" "petclinic_task" {
   family                   = "petclinic"
   network_mode             = "awsvpc"
