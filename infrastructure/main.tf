11e615fa142b1c67de80669615b925ce272bd66f
Caio Queiroz <ccesar.qp@gmail.com>
2024-02-03 09:38:26 -0300

    add autoscaling to ecs

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -81,3 +81,51 @@ resource "aws_ecs_service" "petclinic_service" {
     security_groups  = [aws_security_group.security_group.id]
   }
 }
+
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 10
+  min_capacity       = 1
+  resource_id        = "service/petclinic-cluster/petclinic-service"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+resource "aws_appautoscaling_policy" "cpu_scale_out" {
+  name               = "cpu_scale_out"
+  policy_type        = "StepScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Average"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment          = 1
+    }
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
+  alarm_name          = "high_cpu_utilization"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 80
+  alarm_description   = "This metric monitors ecs cpu utilization"
+  datapoints_to_alarm = 2
+  dimensions = {
+    ClusterName = "petclinic-cluster"
+    ServiceName = "petclinic-service"
+  }
+  actions_enabled = true
+  alarm_actions   = [aws_appautoscaling_policy.cpu_scale_out.arn]
+  ok_actions      = [aws_appautoscaling_policy.cpu_scale_out.arn]
+}
+
