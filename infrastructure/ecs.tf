3908c86d151033fe2684ff954cb308417de4dfeb
Caio Queiroz <ccesar.qp@gmail.com>
2024-02-02 21:43:38 -0300

    add task and service in ECS

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -64,7 +64,7 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 resource "aws_ecs_service" "ecs_service" {
   name            = "petclinic-service"
   cluster         = aws_ecs_cluster.ecs_cluster.id
-  task_definition = aws_ecs_task_definition.ecs_taask_definition.arn
+  task_definition = aws_ecs_task_definition.ecs_task_definition.arn
   desired_count   = 2
 
   network_configuration {
