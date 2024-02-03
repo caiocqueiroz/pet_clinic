f4ed87d552e0e7f0970f783a4540ce918b5a77b7
Caio Queiroz <ccesar.qp@gmail.com>
2024-02-03 07:14:58 -0300

    add task and service in ECS

--- infrastructure/ecs.tf
+++ infrastructure/ecs.tf
@@ -99,16 +99,16 @@ resource "aws_iam_role" "ecsiamrole" {
   name = "ecsTaskExecutionRole"
   assume_role_policy = jsonencode({
     version = "2012-10-17"
-    Statement = [ {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-         }
-       },
-      ]
-    })
-  }
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
+      }
+      },
+    ]
+  })
+}
 
 resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attachment" {
   role       = aws_iam_role.ecsiamrole.name
